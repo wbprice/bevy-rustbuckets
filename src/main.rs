ebbf285a8d97f47d55b9680db7e3a16566128a96
Blaine Price <1wbprice@gmail.com>
2022-05-14 09:18:13 -0400

    hooray, hover events

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_gameboard)
+        .add_system(button_system)
         .run();
 }
 
@@ -69,6 +70,32 @@ fn setup_gameboard(mut commands: Commands, asset_server: Res<AssetServer>) {
                         ..default()
                     });
                 });
+        }
+    }
+}
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, children) in interaction_query.iter_mut() {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "P".to_string();
+                *color = PRESSED_BUTTON.into();
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "H".to_string();
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                text.sections[0].value = "B".to_string();
+                *color = NORMAL_BUTTON.into();
+            }
         }
     }
 }
