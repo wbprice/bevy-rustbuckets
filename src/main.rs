2bb8817a1850af792e1c0772948b98c731e7302a
Blaine Price <1wbprice@gmail.com>
2022-05-13 21:32:24 -0400

    remove everything

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
-//! This is the example that goes to the README.md file. The README.md should be
-//! updated before every release.
-
 use bevy::prelude::*;
-use bevy_prototype_lyon::prelude::*;
 
 fn main() {
     App::new()
-        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
-        .add_plugin(ShapePlugin)
         .add_startup_system(setup_system)
         .run();
 }
 
 fn setup_system(mut commands: Commands) {
-    let shape = shapes::RegularPolygon {
-        sides: 6,
-        feature: shapes::RegularPolygonFeature::Radius(200.0),
-        ..shapes::RegularPolygon::default()
-    };
-
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(GeometryBuilder::build_as(
-        &shape,
-        DrawMode::Outlined {
-            fill_mode: FillMode::color(Color::CYAN),
-            outline_mode: StrokeMode::new(Color::BLACK, 10.0),
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
         },
-        Transform::default(),
-    ));
+        ..default()
+    });
 }
